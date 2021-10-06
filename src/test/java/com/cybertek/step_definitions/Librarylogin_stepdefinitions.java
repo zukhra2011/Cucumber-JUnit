ffcbebfadd754c553c5c9108f961cd87fa6f8ecc
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-10-06 19:05:44 -0400

    Cucumber Project.

---
+++ src/test/java/com/cybertek/step_definitions/Librarylogin_stepdefinitions.java
@@ -0,0 +1,24 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Librarylogin_stepdefinitions {
+    @Given("User is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("User is on the login page");
+    }
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        System.out.println("User enters librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        System.out.println("User enters librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("User should see the dashboard");
+    }
+}
