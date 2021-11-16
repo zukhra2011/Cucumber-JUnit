ab983a590b39caecbde423b29a7d9abfc5fd706f
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-11-16 08:48:46 -0500

    Apache Poi

--- src/test/java/com/cybertek/tests/ExcelRead.java
+++ src/test/java/com/cybertek/tests/ExcelRead.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
@@ -19,5 +20,27 @@ public class ExcelRead {
         //workbook>sheet>row>cell
         //1-Create a workbook
         XSSFWorkbook workbook=new XSSFWorkbook(fileInputStream);
+        //2-We need to get the specific sheet from currently opened workbook
+        XSSFSheet sheet= workbook.getSheet("Employees");
+        //3- Select row and cell
+        //Print out Steven's Cell
+        //Indexes start from 0
+        System.out.println("sheet.getRow(1).getCell(0)="+ sheet.getRow(1).getCell(0));
+        //Print out "Singer" from the sheet
+        System.out.println(sheet.getRow(3).getCell(2));
+        //Return the count of used cells only
+        // If there are cells not used they will not be counted.
+        //Counts from 1.
+        int usedRows=sheet.getPhysicalNumberOfRows();
+        System.out.println(usedRows);
+        //Returns the number from the top cel to bottom ceel
+        int lastUsedRow=sheet.getLastRowNum();
+        //TODO:1-Create a logic to Print out Neena's name dynamically
+        for(int rownum=0; rownum<usedRows; rownum++){
+            System.out.println(sheet.getRow(rownum).getCell(0).equals("Neena"));
+        }
+
+
+
     }
 }
